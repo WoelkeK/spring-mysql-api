cc58c2412ee9e2594215e4a0e127d8fc7ba30cd3
Krzysztof <krzysiek@woelke.pl>
2022-12-17 19:40:56 +0100

    Initial structure of project

---
+++ src/main/java/pl/woelke/springmysqlserver/SpringMysqlServerApplication.java
@@ -0,0 +1,13 @@
+package pl.woelke.springmysqlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlServerApplication.class, args);
+	}
+
+}
